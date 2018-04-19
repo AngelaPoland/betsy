56f6457d4f25fbcaa44b8fbe19f54ccf3ce3da6c
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-19 16:04:46 -0700

    added orders and merchants controllers for baseline

---
+++ app/controllers/merchants_controller.rb
@@ -0,0 +1,10 @@
+class MerchantsController < ApplicationController
+  def account_page
+  end
+
+  def order_fulfillment
+  end
+
+  def products_manager
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get 'merchants/account_page'
+
+  get 'merchants/order_fulfillment'
+
+  get 'merchants/products_manager'
+
+  get 'orders/show'
+
+  get 'orders/create'
+
+  get 'orders/update'
+
+  get 'orders/checkout'
+
+  get 'orders/paid'
+
+  get 'orders/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
