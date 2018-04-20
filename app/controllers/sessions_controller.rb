6151443465d9d89fb6b92e011db6314f0c926501
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-20 16:42:44 -0700

    adjusted nav link to work with temporary paths

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :require_login, only: [:create]
+  # skip_before_action :require_login, only: [:create]
 
   def login
     auth_hash = request.env['omniauth.auth']
