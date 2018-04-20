4e1e336401a0b39ad2efea0eda031b948bb74c51
AngelaPoland <angelalpoland@gmail.com>
2018-04-20 16:19:55 -0700

    messing with application view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :require_login
+  # before_action :require_login
 
   def current_merchant
     @current_merchant ||= Merchant.find(session[:merchant_id]) if session[:merchant_id]
   end
 
   def require_login
-    if current_user.nil?
+    if current_merchant.nil?
       flash[:error] = "You must be logged in to view this section"
       redirect_to session_path
     end
