46b71dac1f5fddd3f1c619e225e34d65eb3b6789
Madaleine Shields <maddiev.shields@gmail.com>
2018-04-19 17:49:03 -0700

    define relationships in Product Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,11 @@
 class Product < ApplicationRecord
+  has_many :order_products
+  has_and_belongs_to_many :catergories
+  has_many :reviews
+  belongs_to :merchant
 
   validates :name, presence: true, uniqueness: true
-  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
   validates_numericality_of :inventory, :only_integer => true, :greater_than_or_equal_to => 0
 
 end
