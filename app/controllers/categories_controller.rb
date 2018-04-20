7676802c7d0fbe74c262bc767f1fb2d0c79d94a1
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-20 16:07:09 -0700

    filled out category action methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,5 +4,17 @@ class CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new
+    # needs to validate that a current merchant is present
+    # add if statement for Merchant
+    @category.category_name = params[:category][:category_name]
+    if @category.save
+      flash[:success] = "created new category"
+      # needs a different path when we are further along
+      redirect_to products_path
+    else
+      flash[:error] = @category.errors
+      render :new
+    end
   end
 end
