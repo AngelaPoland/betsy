12a19ad81c2b0eb7e8c0a8d79f5f13f3699516dd
AngelaPoland <angelalpoland@gmail.com>
2018-04-20 13:12:35 -0700

    added sessions controller login/logout method and helper method in merchant user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,36 @@
 class SessionsController < ApplicationController
   def login
     auth_hash = request.env['omniauth.auth']
+
+    if auth_hash['uid']
+      @merchant = Merchant.find_by(uid: auth_hash[:uid], provider: 'github')
+
+      if @merchant.nil?
+        #its a new user, we need to MAKE a new user
+        @merchant = Merchant.build_from_github(auth_hash)
+        successful_save = @merchant.save
+        if successful_save
+          flash[:success] = "Logged in Successfully. Welcome #{@merchant.username}"
+          session[:merchant_id] = @merchant.id
+          redirect_to root_path
+        else
+          flash[:error] = "Some error happened in Merchant creation"
+          redirect_to root_path
+        end
+      else
+        flash[:success] = "Logged in successfully"
+        session[:merchant_id] = @merchant.id
+        redirect_to root_path
+      end
+    else
+      flash[:error] = "Logging in through Github not successful"
+      redirect_to root_path
   end
 
   def logout
+    session[:merchant_id] = nil
+    flash[:status] = :success
+    flash[:result_text] = "Successfully logged out"
+    redirect_to root_path
   end
 end
