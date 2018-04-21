7c7ed15c2079563b01925f11ad13bc0deac86f10
Madaleine Shields <maddiev.shields@gmail.com>
2018-04-20 17:17:44 -0700

    move current_merchant method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # before_action :require_login
-  before_action :current_merchant
-
   def current_merchant
-    @current_merchant = Merchant.find_by(id: session[:merchant_id]) 
+    @current_merchant ||= Merchant.find(session[:merchant_id]) if session[:merchant_id]
   end
 
-  def require_login
-    if current_merchant.nil?
-      flash[:error] = "You must be logged in to view this section"
-      redirect_to session_path
-    end
-  end
 end
