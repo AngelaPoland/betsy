c71e6eca33c3f4ada21ae22803940385d85c634b
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-20 17:23:33 -0700

    potentially fixed login issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :current_merchant
+
   def current_merchant
     @current_merchant ||= Merchant.find(session[:merchant_id]) if session[:merchant_id]
   end
