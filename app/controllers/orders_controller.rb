0ccfe8d5431fd4ef3f6ccece7047c49d36f69628
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-22 16:52:51 -0700

    added logic to the paid action in the order controller, updated the paid view as the confirmation page and added temporary content to test the results, and update the form path in checkout view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,9 +12,35 @@ class OrdersController < ApplicationController
   end
 
   def paid #submit after checkout
-    
+    if @current_cart.update(billing_params)
+      @current_cart.order_products.each do |order_product|
+        order_product.update_attributes(status: "paid")
+      end
+      @current_cart.update_attributes(status: "paid")
+      flash[:success] = "Order received! Thank you for your purchase."
+      session[:order_id] = nil
+    elsif @current_cart.errors.any?
+      flash.now[:error] = @current_cart.errors
+      render :checkout
+    else
+      flash[:alert] = "Something went wrong, and we couldn't process your order."
+      render :checkout
+    end
   end
 
   def destroy #this clears the cart before order has gone into paid status
+  end
+
+  private
+
+  def billing_params
+    params.require(:order).permit(
+      :billing_address,
+      :billing_name,
+      :billing_num,
+      :billing_exp,
+      :billing_cvv,
+      :billing_zipcode
+    )
   end
 end
