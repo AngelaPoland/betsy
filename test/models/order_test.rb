0512354192f084b6c69ad47c3a16624640a99aa7
Madaleine Shields <maddiev.shields@gmail.com>
2018-04-23 11:33:42 -0700

    Write validations tests for order_test

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,7 +3,36 @@ require "test_helper"
 describe Order do
   let(:order) { Order.new }
 
-  it "must be valid" do
-    value(order).must_be :valid?
+  describe "validations" do
+    it "must be valid" do
+      order_one = orders(:order_one)
+      order_one.valid?.must_equal true
+    end
+
+    it "must be valid when billing info is blank and status is pending" do
+      order_five = orders(:order_five)
+      order_five.valid?.must_equal true
+    end
+
+    it "must be valid when billing info is filled and status is paid or complete" do
+      order_four = orders(:order_four)
+      order_four.valid?.must_equal true
+    end
+
+    it "must be invalid when billing info is blank and status is paid or complete" do
+      order_five = orders(:order_five)
+      order_five.status = "paid"
+      order_five.valid?.must_equal false
+    end
+
+    it "must be invalid when billing info is filled and status is pending" do
+      order_four = orders(:order_four)
+      order_four.status = "pending"
+      order_four.valid?.must_equal false
+    end
+
+
   end
+
+
 end
