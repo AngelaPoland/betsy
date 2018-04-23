b115aaa103b6448f9ddbe3e96463731366c0b688
Nora Peters <norapeters5@gmail.com>
2018-04-23 11:09:45 -0700

    adds validation and relation tests for Review model. all tests passing

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :product
 
   validates :rating, presence: true
-  validates :rating, numericality: { only_integer: true, in: 1..5 }
+  validates_inclusion_of :rating, numericality: true, in: (1..5)
 
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -3,7 +3,53 @@ require "test_helper"
 describe Review do
   let(:review) { Review.new }
 
-  it "must be valid" do
-    value(review).must_be :valid?
+  describe "validations" do
+    it "must be valid" do
+      review1 = reviews(:review1)
+      review1.rating.class.must_equal Integer
+      review1.valid?.must_equal true
+    end
+
+    it "must be invalid if rating is blank" do
+      review2 = reviews(:review2)
+      review2.rating = nil
+      review2.valid?.must_equal false
+    end
+
+    it "must be invalid if rating is a string" do
+      review3 = reviews(:review3)
+      review3.rating = 'mystring'
+      review3.valid?.must_equal false
+    end
+
+    it "must be invalid if rating is less than 1" do
+      review4 = reviews(:review4)
+      review4.rating = 0
+      review4.valid?.must_equal false
+    end
+
+    it "must be invalid if rating is greater than 5" do
+      review1 = reviews(:review1)
+      review1.rating = 6
+      review1.valid?.must_equal false
+    end
+
+    it "must be valid if description is nil" do
+      review2 = reviews(:review2)
+      review2.description = nil
+      review2.valid?.must_equal true
+    end
+  end
+
+  describe 'relations' do
+
+    it 'must have a Product' do
+      review1 = reviews(:review1)
+      review1.must_respond_to :product
+      review1.product.must_be_kind_of Product
+      review1.product.name.must_equal "man bun"
+      review1.product.must_equal products(:manbun)
+    end
+
   end
 end
