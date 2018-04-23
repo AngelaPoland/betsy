5e87f73f406ee5d22b52a21b9e10540549fc61c5
AngelaPoland <angelalpoland@gmail.com>
2018-04-23 16:40:43 -0700

    categories model tests for validations only

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,35 @@
 require "test_helper"
 
 describe Category do
-  let(:category) { Category.new }
+  let(:category) { categories(:clothes) }
 
-  it "must be valid" do
-    value(category).must_be :valid?
+  describe "validations" do
+
+    it "must be valid" do
+      category.must_be :valid?
+    end
+
+    it "must be invalid without a category_name" do
+      category.category_name = nil
+      category.valid?.must_equal false
+    end
+
+    it "must be invalid if it has a duplicate name" do
+      category2 = categories(:extra)
+      category2.category_name = "Clothing"
+      category2.valid?.must_equal false
+    end
+
   end
+
+  describe "relationships" do
+
+    it "has and belongs to products" do
+      category.must_respond_to :product
+      
+    end
+
+  end
+
+
 end
