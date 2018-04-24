c494370825391b54379d7553141a8d943f3eaec0
Nora Peters <norapeters5@gmail.com>
2018-04-24 10:54:15 -0700

    adds tests for reviews controller

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,14 +1,54 @@
 require "test_helper"
 
 describe ReviewsController do
-  it "should get new" do
-    get reviews_new_url
-    value(response).must_be :success?
+  let(:product) { products(:kombucha) }
+
+  describe "new" do
+    it "succeeds" do
+      get new_product_review_path(product.id)
+      must_respond_with :success
+    end
   end
 
-  it "should get create" do
-    get reviews_create_url
-    value(response).must_be :success?
-  end
+  describe "create" do
+    it "creates a new product review for valid data" do
+      proc {
+        post product_reviews_path(product.id), params: {
+          review: {
+            rating: 1,
+            description: "Super bad"
+          }
+        }
+      }.must_change 'Review.count', 1
 
+      must_respond_with :redirect
+      must_redirect_to product_path(product.id)
+    end
+
+    it "does not create a new review with bogus data" do
+      proc {
+        post product_reviews_path(product.id), params: {
+          review: {
+            rating: "one",
+            description: "Super bad"
+          }
+        }
+      }.wont_change 'Review.count'
+    end
+
+    it "does not allow merchant to review her own products" do
+      merchant = merchants(:mads)
+      login(merchant)
+      proc {
+        post product_reviews_path(product.id), params: {
+          review: {
+            rating: 4,
+            description: "Super good"
+          }
+        }
+      }.wont_change 'Review.count'
+      must_respond_with :redirect
+      must_redirect_to product_path(product.id)
+    end
+  end
 end
