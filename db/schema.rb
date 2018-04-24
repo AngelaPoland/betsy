11b8f2739056e907c19b9ab4bf4e08e981603aa6
Nora Peters <norapeters5@gmail.com>
2018-04-23 17:45:08 -0700

    updated schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180421044914) do
+ActiveRecord::Schema.define(version: 20180424001206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 20180421044914) do
   create_table "categories_products", force: :cascade do |t|
     t.bigint "product_id"
     t.bigint "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["category_id"], name: "index_categories_products_on_category_id"
     t.index ["product_id"], name: "index_categories_products_on_product_id"
   end
