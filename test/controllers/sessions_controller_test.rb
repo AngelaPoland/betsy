b12856f66fa7829038ff4c39961533c7b37e99cd
AngelaPoland <angelalpoland@gmail.com>
2018-04-25 16:54:15 -0700

    finished sessions controller tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,14 +1,61 @@
 require "test_helper"
 
 describe SessionsController do
-  it "should get login" do
-    get sessions_login_url
-    value(response).must_be :success?
-  end
+  describe "login" do
 
-  it "should get logout" do
-    get sessions_logout_url
-    value(response).must_be :success?
-  end
+    it "logs in an existing user and redirects to the root route" do
+      existing_merchant = merchants(:mads)
 
+      start_count = Merchant.count
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(existing_merchant))
+      # here ^ mock_auth is not a path or method thats being called - its part of the overall way OmniAuth is used..?
+
+      get auth_callback_path(existing_merchant.provider)
+
+      Merchant.count.must_equal start_count
+      must_redirect_to root_path
+      session[:merchant_id].must_equal existing_merchant.id
+    end
+
+    it "must create a new user and redirects to the root route" do
+      new_merchant = Merchant.new(
+        provider: 'github',
+        uid: 666,
+        username: 'new test merchant',
+        email: 'test@test.com'
+      )
+
+      proc {
+      login(new_merchant) }.must_change 'Merchant.count', 1
+
+      must_redirect_to root_path
+    end
+
+    it "fails if a user is already logged in and a user attempts to log in" do
+      existing_merchant = merchants(:mads)
+      login(existing_merchant)
+
+      proc {
+      login(existing_merchant) }.wont_change 'Merchant.count'
+
+      must_redirect_to root_path
+
+    end
+
+    it "redirects to the root route and doesn't create new user if given invalid user data" do
+      bad_merchant = Merchant.new(
+        provider: 'github',
+        uid: 666,
+        email: 'test@test.com'
+      )
+
+      proc {
+      login(bad_merchant) }.must_change 'Merchant.count', 0
+
+      must_redirect_to root_path
+    end
+
+
+  end
 end
