7e84a163d606fb9b74f31635e841f6799592cf9b
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-25 23:33:44 -0700

    refactored categories controller tests, added more tests for edge cases (including optional one that is failing in create action, test 004) and updated products.yml for category tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -6,40 +6,95 @@ describe CategoriesController do
 
   describe "new" do
 
-    it "succeeds" do
+    it "successfully gets new view for form" do
       login(ange)
-      get new_category_path
-      must_respond_with :success
 
+      get new_category_path
+
+      must_respond_with :success
+    end
+
+    it "redirects to root view if no user has logged in" do
+      get new_category_path
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
+
+    it "redirects to root view if a user has logged out" do
+      # inspired from Media Ranker revisited
+      # is this necessary?
     end
 
   end
 
   describe "create" do
 
-    it "creates a new category with valid data" do
+    it "successfully creates a new category with valid data while logged in" do
       login(ange)
+      product = Product.find_by(name: "sage")
+
       proc {
         post categories_path, params: { category: { category_name: "libations",
-        products: "kombucha"   }  }
+        product_ids: [product.id] }  }
+      }.must_change 'Category.count', 1
+
+      product.categories.length.must_equal 3
+      must_respond_with :redirect
+      must_redirect_to products_manager_path
+    end
+
+    it "successfully creates a new category without any products chosen" do
+      login(ange)
+
+      proc {
+        post categories_path, params: { category: { category_name: "libations",
+        product_ids: [] }  }
       }.must_change 'Category.count', 1
 
       must_respond_with :redirect
       must_redirect_to products_manager_path
-
     end
 
     it "will not create a new category with bogus data" do
       login(ange)
+      product = Product.find_by(name: "sage")
+
       proc {
         post categories_path, params: { category: { category_name: " ",
-        products: "kombucha"   }  }
+        product_ids: [product.id]   }  }
       }.wont_change 'Category.count'
-      must_respond_with :error
 
+      product.categories.length.must_equal 2
+      must_respond_with :error
     end
 
+    it "doesn't add new category to products that do not belong to current user" do
+      login(ange)
+      product = Product.find_by(name: "kombucha")
 
+      proc {
+        post categories_path, params: { category: { category_name: "libations",
+        product_ids: [product.id] }  }
+      }.must_change 'Category.count', 1
+
+      product.categories.length.must_equal 2
+      must_respond_with :redirect
+      must_redirect_to products_manager_path
+    end
+
+    it "redirects to root view if no user present and doesn't save to model" do
+      product = Product.find_by(name: "sage")
+
+      proc {
+        post categories_path, params: { category: { category_name: "libations",
+        product_ids: [product.id]   }  }
+      }.wont_change 'Category.count'
+
+      product.categories.length.must_equal 2
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
 
   end
 end
