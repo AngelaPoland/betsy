6a121a1c1ba89426aeadaa0a503a523b6c936b6b
Madaleine Shields <maddiev.shields@gmail.com>
2018-04-25 18:39:03 -0700

    apply require_login to merchants controller and delete comments from sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,12 @@
 class SessionsController < ApplicationController
-  # skip_before_action :require_login, only: [:create]
-
-  # def failure
-  #   auth_hash = request.env['omniauth.auth']
-  #
-  #   logger.debug "MADE IT TO FAILURE"
-  #   logger.debug "Auth_hash = #{auth_hash}"
-  #   logger.debug "params = #{params}"
-  #
-  # end
 
   def login
     auth_hash = request.env['omniauth.auth']
-
-    # logger.debug "MADE IT TO LOGIN"
-    # logger.debug "Auth_hash = #{auth_hash}"
 
     if auth_hash['uid']
       @merchant = Merchant.find_by(uid: auth_hash[:uid], provider: 'github')
 
       if @merchant.nil?
-        #its a new user, we need to MAKE a new user
         @merchant = Merchant.build_from_github(auth_hash)
         successful_save = @merchant.save
         if successful_save
