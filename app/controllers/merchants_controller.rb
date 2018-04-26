3931c0b6bad2d2dbd77bfe5c34b71a0b03ecea9d
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-26 00:27:44 -0700

    added tests for all actions in merchant controller, a few stubs that may be unnecessary to test

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -6,15 +6,20 @@ class MerchantsController < ApplicationController
   end
 
   def order_fulfillment
+    status = params[:status]
+    valid_status = ["paid", "shipped", "cancelled"]
     @order_products = []
     @merchant_orders = []
     order_products = OrderProduct.all
-    order_products = OrderProduct.status(params[:status]) if params[:status]
+    order_products = OrderProduct.status(params[:status]) if status && valid_status.include?(status)
     order_products.each do | order_product |
       @order_products << order_product if order_product.merchant == @current_merchant
     end
     @current_merchant.products.each do |product|
       product.order_products.each { |order_product| @merchant_orders << order_product }
+    end
+    if !valid_status.include?(status)
+      flash[:alert] = "Try a real status next time"
     end
   end
 
--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,19 +1,114 @@
 require "test_helper"
 
 describe MerchantsController do
-  it "should get account_page" do
-    get merchants_account_page_url
-    value(response).must_be :success?
+  let(:meka) { merchants(:kat) }
+  describe 'account_page' do
+
+    it 'successfully gets account_page view if merchant is logged in' do
+      login(meka)
+
+      get account_page_path
+
+      must_respond_with :success
+    end
+
+    it 'redirects to root if no merchant is logged in' do
+      get account_page_path
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
+
+    it 'redirects to root if merchant has logged out' do
+      # based on Media Ranker Revisited
+      # necessary?
+    end
   end
 
-  it "should get order_fulfillment" do
-    get merchants_order_fulfillment_url
-    value(response).must_be :success?
+  describe 'order_fulfillment' do
+    it 'successfully gets order_fulfillment view if merchant is logged in' do
+      login(meka)
+
+      get order_fulfillment_path
+
+      must_respond_with :success
+    end
+
+    it 'successfully gets order_fulfillment view if merchant has no orders' do
+      meka.products.each do | product |
+        if !product.order_products.nil?
+          product.order_products.each { | order | order.destroy }
+        end
+      end
+      login(meka)
+
+      get order_fulfillment_path
+
+      must_respond_with :success
+    end
+
+    it 'successfully gets filtered products_manager view if valid filter applied' do
+      login(meka)
+
+      get products_manager_path, params: { status: "paid" }
+
+      must_respond_with :success
+    end
+
+    it 'successfully gets all products_manager view if invalid filter applied' do
+      login(meka)
+
+      get products_manager_path, params: { status: "blah" }
+
+      must_respond_with :success
+      # this seems unnecessary in the way it is tested
+      # is there another way to test with scope/filtered views?
+    end
+
+    it 'redirects to root if no merchant is logged in' do
+      get order_fulfillment_path
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
+
+    it 'redirects to root if merchant has logged out' do
+      # based on Media Ranker Revisited
+      # necessary?
+    end
   end
 
-  it "should get products_manager" do
-    get merchants_products_manager_url
-    value(response).must_be :success?
+  describe 'products_manager' do
+    it 'successfully gets products_manager view if merchant is logged in' do
+      login(meka)
+
+      get products_manager_path
+
+      must_respond_with :success
+    end
+
+    it 'successfully gets products_manager view if merchant has no products' do
+      meka.products.each do | product |
+        product.destroy
+      end
+      login(meka)
+
+      get products_manager_path
+
+      must_respond_with :success
+    end
+
+    it 'redirects to root if no merchant is logged in' do
+      get products_manager_path
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
+
+    it 'redirects to root if merchant has logged out' do
+      # based on Media Ranker Revisited
+      # necessary?
+    end
   end
 
 end
