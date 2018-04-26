fd62be5d8281e3a6b8e8cfd9ef7c6d1c83321070
Madaleine Shields <maddiev.shields@gmail.com>
2018-04-26 09:26:28 -0700

    edit merchants_controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -20,7 +20,6 @@ class MerchantsController < ApplicationController
 
 
   def order_fulfillment
-    <<<<<<< HEAD
     @merchant_orders = []
     @current_merchant.products.each do |product|
       product.order_products.each do |order_product|
